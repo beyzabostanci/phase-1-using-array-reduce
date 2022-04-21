1bd20293c9303be550b8c588ab992f72469ec46f
beyzabostanci <beyza_bostanc@hotmail.com>
2022-04-21 12:48:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+const totalBatteries = batteryBatches.reduce( function(first, current){
+return  first + current;
+  }, 0);
+
+
+
+
+/*reminder for myself:
+.reduce() executes the callback for each element in turn,
+ passing in the current value of the accumulator and the current element each time.
+ if no initial value is supplied,the first element in the array is used as the starting value.
+  The callback is where we'll push*/
